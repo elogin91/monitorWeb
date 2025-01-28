76cd6d715c7a04f723a6248d5ab35ebd4ee2221c
elogin91 <elo.programacion@gmail.com>
2025-01-28 15:04:56 +0100

    monitor web working with DB

--- src/app/api/check-websites/route.ts
+++ src/app/api/check-websites/route.ts
@@ -6,6 +6,6 @@ const monitorService = new MonitorService();
 // Endpoint para devolver el estado actual de las webs
 export async function GET() {
   await monitorService.refreshStatusesIfNeeded();
-  const statuses = monitorService.getStatuses();
+  const statuses = await monitorService.getStatuses();
   return NextResponse.json(statuses);
 }
--- src/models/webStatus.ts
+++ src/models/webStatus.ts
@@ -24,4 +24,24 @@ const webStatusSchema: Schema = new Schema({
   lastChecked: { type: String, required: true },
 });
 
-export const WebStatus = mongoose.model<IWebStatus>('WebStatus', webStatusSchema);
+// Método que compara la hora de la última comprobación
+webStatusSchema.methods.hasBeenCheckedRecently = function (): boolean {
+  const lastCheckedDate = new Date(this.lastChecked);
+  const currentTime = new Date();
+  const timeDifference = (currentTime.getTime() - lastCheckedDate.getTime()) / 1000; // Diferencia en segundos
+  return timeDifference < 60;
+};
+
+// Método para actualizar los datos del estado
+webStatusSchema.methods.updateStatus = function (
+  status: string,
+  headers: { statusCode: string | number; date: string; contentType: string },
+  responseTime: string
+): void {
+  this.status = status;
+  this.headers = headers;
+  this.responseTime = responseTime;
+  this.lastChecked = new Date().toLocaleString();
+};
+
+export const WebStatus = mongoose.models.WebStatus || mongoose.model('WebStatus', webStatusSchema);
